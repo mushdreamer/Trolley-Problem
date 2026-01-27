5e05060e6dea7db6003376018aa2ac08309d0712
Liang393 <liang393@purdue.edu>
2026-01-26 23:24:13 -0500

    Added Spline prototype
    I added a Spline prototype on the right side of the map, feel free to play it out.
    1.  unattach the camera for development
    2. added a Spline prototype
    3. press 1 to turn left, press 2 to turn right

---
+++ Assets/SplineScript.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.Splines;
+
+public class SplineScriptTest : MonoBehaviour
+{
+    public SplineAnimate anim;
+
+    [Header("Containers (each has ONLY one spline)")]
+    public SplineContainer Trunk;
+    public SplineContainer Left;
+    public SplineContainer Right;
+
+    [Header("Trigger Tags")]
+    public string junctionTag = "SplineTrigger"; // triggers the train to turn left or right
+    public string mergeTag = "MergeTrigger";     // triggers the train to drive back to common path or trunk
+
+    [Header("Choice")]
+    public bool goLeft = true;
+
+    private enum TrackState { OnTrunk, OnLeft, OnRight }
+    private TrackState state = TrackState.OnTrunk;
+
+    void Start()
+    {
+        if (!anim) anim = GetComponent<SplineAnimate>();
+        GoToTrunk(); // start with common path or trunk
+    }
+
+    void Update()
+    {
+        // press 1 to turn left, press 2 to turn right; have to press before trigger box
+        if (Input.GetKeyDown(KeyCode.Alpha1)) goLeft = true;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) goLeft = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // triggers turn left or right here
+        if (other.CompareTag(junctionTag) && state == TrackState.OnTrunk)
+        {
+            if (goLeft) GoToLeft();
+            else GoToRight();
+            return;
+        }
+
+        // 2) triggers anim on common path or trunk
+        if (other.CompareTag(mergeTag) && (state == TrackState.OnLeft || state == TrackState.OnRight))
+        {
+            GoToTrunk();
+            return;
+        }
+    }
+
+    void GoToTrunk()
+    {
+        state = TrackState.OnTrunk;
+        anim.Container = Trunk;
+        SafeRestart();
+    }
+
+    void GoToLeft()
+    {
+        state = TrackState.OnLeft;
+        anim.Container = Left;
+        SafeRestart();
+    }
+
+    void GoToRight()
+    {
+        state = TrackState.OnRight;
+        anim.Container = Right;
+        SafeRestart();
+    }
+
+    void SafeRestart()
+    {
+        //
+        try
+        {
+            anim.Restart(true);
+            return;
+        }
+        catch { }
+
+        //
+        anim.enabled = false;
+        anim.enabled = true;
+    }
+}
